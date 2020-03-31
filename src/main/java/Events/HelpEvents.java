dbe407d94a761de24f188921d9e3ef3c75db291e
Prajwal47 <26029731+Prajwal47@users.noreply.github.com>
2020-03-31 14:38:33 +0530

    updated stock event & Help event looks

--- src/main/java/Events/HelpEvents.java
+++ src/main/java/Events/HelpEvents.java
@@ -17,19 +17,21 @@ public class HelpEvents extends ListenerAdapter {
         if (Message_sent.equalsIgnoreCase("!help")) {
             if (!event.getMember().getUser().isBot()) {
 
-                eb.setTitle("Help for Javabot");
+                eb.setTitle("***Help for Salty***");
                 eb.setColor(Color.CYAN);
-                eb.addField("prefix", "!", false);
+                eb.addField("prefix", "!", true);
                 //  eb.addField("","",false);
-                eb.setAuthor("CinematicCow#8325", "https://discord.gg/WcDpNQB", "https://i.pinimg.com/736x/6c/fa/a9/6cfaa9135276c3a207e560f67953fa0e.jpg");
-                eb.addField("for Spotify", "spotify", false);
-                eb.addField("for Minecraft", "minecraft", false);
-                eb.addField("for Netflix", "netflix", false);
-                eb.addField("to donate", "donate", false);
-                eb.addField("!edit_bot ", "to humiliate yourself", false);
-                eb.setFooter("to execute a command !command without space is necessary to work", "https://www.snopes.com/tachyon/2007/09/ermeypress.jpg?fit=1080,540");
-                eb.setImage("https://www.snopes.com/tachyon/2007/09/ermeypress.jpg?fit=1080,540");
-                eb.setThumbnail("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FZS_P8oEjxTA%2Fmaxresdefault.jpg&f=1&nofb=1");
+                eb.setAuthor(event.getGuild().getName().toString(), "https://discord.gg/WcDpNQB");
+                eb.addField("To generate Spotify", " spotify", false);
+                eb.addField("To generate Steam", " steam", false);
+                eb.addField("To generate Nord Vpn", " nordvpn", false);
+                eb.addField("To check the remaning stock of avilable accounts", " stock", false);
+                eb.addField("To donate", " donate", false);
+                //eb.addField("!edit_bot ", "to humiliate yourself", false);
+                eb.addField("Useless commands list", " helpme", false);
+                eb.setFooter("to execute a command '!command' WITHOUT SPACE is necessary to work" );
+                eb.setFooter("All of the above commands will run on their specified channels" );
+                eb.setThumbnail(event.getGuild().getIconUrl());
                 event.getChannel().sendMessage(eb.build()).queue();
 
                 // event.getChannel().sendMessage("Bugger off "+name+". I need some rest"+"\n"+"Come back later!").queue();
